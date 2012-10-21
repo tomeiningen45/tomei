5043f556758502e109689ec6766403f5f955bed4
tomei.ningen45@gmail.com <tomei.ningen45@gmail.com@10d319c2-8b8c-a289-d5c3-688514be38f6>
2012-10-21 04:09:42 +0000

    added hk yahoo

--- trunk/web2rss/sync.sh
+++ trunk/web2rss/sync.sh
@@ -3,7 +3,7 @@ while true; do
     echo start===========`date`
     tclsh 6park.tcl
     scp data/6park.xml $WEB2RSSROOT/test2.xml
-    tclsh hkyahoo.tcl
+    tclsh yahoohk.tcl
     scp data/hkyahoo.xml $WEB2RSSROOT/test3.xml
     echo done============`date`
     sleep 3600
