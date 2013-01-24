55f19c367bab7b7f715d56a5635c033537d5d487
tomei.ningen45 <tomei.ningen45@gmail.com>
2013-01-24 03:22:28 +0000

    avoid getting blocked by hanging scp on win32

--- web2rss/sync.sh
+++ web2rss/sync.sh
@@ -9,31 +9,40 @@ fi
     echo start===========`date`
 
     tclsh 6park.tcl
-    $SCP data/6park.xml $root/test2.xml
+    echo wait to scp; sleep 5;
+    $SCP data/6park.xml $root/test2.xml &
 
     #tclsh wforum.tcl
-    #$SCP data/wforum.xml $root/wforum.xml
+    #echo wait to scp; sleep 5;
+    $SCP data/wforum.xml $root/wforum.xml &
 
     tclsh yahoohk.tcl
-    $SCP data/hkyahoo.xml $root/test3.xml
+    echo wait to scp; sleep 5;
+    $SCP data/hkyahoo.xml $root/test3.xml &
 
     tclsh cnbeta.tcl
-    $SCP data/cnbeta.xml $root/cnbeta.xml
+    echo wait to scp; sleep 5;
+    $SCP data/cnbeta.xml $root/cnbeta.xml &
 
     tclsh iza.tcl
-    $SCP data/iza.xml $root/iza.xml
+    echo wait to scp; sleep 5;
+    $SCP data/iza.xml $root/iza.xml &
 
     tclsh yahoofn_top.tcl 
-    $SCP data/yahoofn_*.xml $root/
+    echo wait to scp; sleep 5;
+    $SCP data/yahoofn_*.xml $root/ &
 
     tclsh bloomberg_top.tcl 
-    $SCP data/bloomberg_*.xml $root/
+    echo wait to scp; sleep 5;
+    $SCP data/bloomberg_*.xml $root/ &
 
     tclsh zasshi.tcl 
-    $SCP data/zasshi*.xml $root/rss/
+    echo wait to scp; sleep 5;
+    $SCP data/zasshi*.xml $root/rss/ &
 
     tclsh gooblog_top.tcl 
-    scp data/gooblog_*.xml $WEB2RSSROOT/rss/
+    echo wait to scp; sleep 5;
+    $SCP data/gooblog_*.xml $WEB2RSSROOT/rss/ &
 
     tclsh delete_old_files.tcl
 
