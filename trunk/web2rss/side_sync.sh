ebf2edabd6440021aa650a7b2f706f8c1ad56f2b
tomei.ningen45@gmail.com <tomei.ningen45@gmail.com@10d319c2-8b8c-a289-d5c3-688514be38f6>
2013-06-23 00:34:00 +0000

    fix

--- trunk/web2rss/side_sync.sh
+++ trunk/web2rss/side_sync.sh
@@ -1,6 +1,9 @@
 echo SIDE_SYNC start ===========`date`
 
-#tclsh cnbeta_comments.tcl
-tclsh yahoohk_comments.tcl
+if test -f sync_cnbeta.file; then
+    tclsh cnbeta_comments.tcl
+else
+    tclsh yahoohk_comments.tcl
+fi
 
 echo SIDE_SYNC done============`date`
