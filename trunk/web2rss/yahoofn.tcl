2eb1f91a58ccd958f0e3379195b30de9873fd005
tomei.ningen45@gmail.com <tomei.ningen45@gmail.com@10d319c2-8b8c-a289-d5c3-688514be38f6>
2014-05-05 01:05:10 +0000

    merged yahoofn

--- trunk/web2rss/yahoofn.tcl
+++ trunk/web2rss/yahoofn.tcl
@@ -14,7 +14,7 @@ source $instdir/rss.tcl
 #----------------------------------------------------------------------
 
 proc update {} {
-    global datadir argv env
+    global datadir argv env map
 
     set name [lindex $argv 0]
     set title [lindex $argv 1]
@@ -42,6 +42,14 @@ proc update {} {
     regsub -all DESC        $out $tit out
     regsub -all TITLE       $out $tit out
 
+foreach item $map {
+    set name [lindex $item 0]
+    set title [lindex $item 1]
+    set url [lindex $item 2]
+
+    puts ""
+    puts ================================================================================$name
+    puts ""
 
     set data [wget $url]
 
@@ -67,6 +75,12 @@ proc update {} {
         if {![regexp {/([^/]+[.]html)$} $link dummy localname]} {
             continue
         }
+
+        if {[info exists seen($link)]} {
+            puts exist=$link
+            continue
+        }
+        set seen($link) 1
 
         set fname [getcachefile $localname]
         set data [getfile $link $localname]
@@ -101,17 +115,47 @@ proc update {} {
             continue
         }
 
+        regsub -all "\n<p><strong\[^>\]*>Related:</strong>\[^\n\]*</p>" $data "\n" data
+        regsub -all {<p><strong\[^>\]*>Related:</strong>[^<]*<a href=[^>]*>[^<]+</a>} $data "" data
+
         puts $title==$link
 
         append out [makeitem $title $link $data $date]
     }
-
+}
     append out {</channel></rss>}
 
-    set fd [open ${datadir}_${name}.xml w+]
+    set fd [open ${datadir}_main.xml w+]
     fconfigure $fd -encoding utf-8
     puts -nonewline $fd $out
     close $fd
+}
+
+set map {
+    {
+    main "Business & Finance News | Yahoo Finance" 
+        http://finance.yahoo.com/news/;_ylt=AmEFggb5wb3RosqZH6iKkXKhuYdG;_ylu=X3oDMTI0ZzNjZmE2BG1pdANTdWJzY3JpYmUgYW5kIFNpdGUgSW5kZXggVG9wIFN0b3JpZXMEcG9zAzYEc2VjA01lZGlhUlNTRWRpdG9yaWFs;_ylg=X3oDMTFzdHZoMTdhBGludGwDdXMEbGFuZwNlbi11cwRwc3RhaWQDBHBzdGNhdANuZXdzfHByb3ZpZGVycwRwdANzZWN0aW9ucw--;_ylv=3?format=rss
+    }
+    {
+    commodity "Commodities News and Information | Yahoo Finance"
+        http://finance.yahoo.com/news/category-commodities/rss;_ylt=ApXoeBtSjABggoK1add9fmWhuYdG;_ylu=X3oDMTI0M29vYW91BG1pdANTdWJzY3JpYmUgYW5kIFNpdGUgSW5kZXggVVMgTWFya2V0cwRwb3MDMTgEc2VjA01lZGlhUlNTRWRpdG9yaWFs;_ylg=X3oDMTFzdHZoMTdhBGludGwDdXMEbGFuZwNlbi11cwRwc3RhaWQDBHBzdGNhdANuZXdzfHByb3ZpZGVycwRwdANzZWN0aW9ucw--;_ylv=3
+    }
+    {
+    economy "Economy, Government & Policy News | Yahoo Finance"
+        http://finance.yahoo.com/news/category-economy-govt-and-policy/rss;_ylt=ApsOxf3kAB7rj2QMqxWDwS.huYdG;_ylu=X3oDMTI1bHFtOWR2BG1pdANTdWJzY3JpYmUgYW5kIFNpdGUgSW5kZXggR2VuZXJhbCBOZXdzBHBvcwM2BHNlYwNNZWRpYVJTU0VkaXRvcmlhbA--;_ylg=X3oDMTFzdHZoMTdhBGludGwDdXMEbGFuZwNlbi11cwRwc3RhaWQDBHBzdGNhdANuZXdzfHByb3ZpZGVycwRwdANzZWN0aW9ucw--;_ylv=3
+    }
+    {
+    international "International"
+        http://finance.yahoo.com/news/category-economy/rss;_ylt=AuZRgW6lfCsYGR3_d_MoBhqhuYdG;_ylu=X3oDMTI2cnR0OGtvBG1pdANTdWJzY3JpYmUgYW5kIFNpdGUgSW5kZXggR2VuZXJhbCBOZXdzBHBvcwMxMgRzZWMDTWVkaWFSU1NFZGl0b3JpYWw-;_ylg=X3oDMTFzdHZoMTdhBGludGwDdXMEbGFuZwNlbi11cwRwc3RhaWQDBHBzdGNhdANuZXdzfHByb3ZpZGVycwRwdANzZWN0aW9ucw--;_ylv=3
+    }
+    {
+    investing "Investing Ideas & Strategies | Yahoo Finance"
+        http://finance.yahoo.com/news/category-ideas-and-strategies/rss;_ylt=AkPljMBDO2lA7YxU15jlwSyhuYdG;_ylu=X3oDMTI0dTRkanY1BG1pdANTdWJzY3JpYmUgYW5kIFNpdGUgSW5kZXggVVMgTWFya2V0cwRwb3MDMjcEc2VjA01lZGlhUlNTRWRpdG9yaWFs;_ylg=X3oDMTFzdHZoMTdhBGludGwDdXMEbGFuZwNlbi11cwRwc3RhaWQDBHBzdGNhdANuZXdzfHByb3ZpZGVycwRwdANzZWN0aW9ucw--;_ylv=3
+    }
+    {
+    tech "Technology"
+        http://finance.yahoo.com/news/sector-technology/rss;_ylt=Aqdb_Je_9Tx83qfarY3eOmmhuYdG;_ylu=X3oDMTIxbGJnMjFpBG1pdANTdWJzY3JpYmUgYW5kIFNpdGUgSW5kZXggU2VjdG9ycwRwb3MDMjEEc2VjA01lZGlhUlNTRWRpdG9yaWFs;_ylg=X3oDMTFzdHZoMTdhBGludGwDdXMEbGFuZwNlbi11cwRwc3RhaWQDBHBzdGNhdANuZXdzfHByb3ZpZGVycwRwdANzZWN0aW9ucw--;_ylv=3
+    }
 }
 
 update
