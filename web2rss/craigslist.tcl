75395becce0308fb6f6bcb73b0c12402ea0c94bd
tomei.ningen45 <tomei.ningen45@gmail.com>
2014-06-22 15:18:20 +0000

    relaxed year in craig

--- web2rss/craigslist.tcl
+++ web2rss/craigslist.tcl
@@ -249,7 +249,7 @@ proc update {} {
 
             set link http://${s}.craigslist.org$link
 
-            set year_checker {"$year" == "" || $year >= 1999}
+            set year_checker {"$year" == "" || ($year >= 1999 && $year <= 2004) || ($year > 2010)}
 
             set title [convert_title $title $s $year_checker]
             if {"$title" == ""} {
