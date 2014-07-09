b103c1873c6d868d0db2cf835394abb81dd9dc47
tomei.ningen45@gmail.com <tomei.ningen45@gmail.com@10d319c2-8b8c-a289-d5c3-688514be38f6>
2014-07-09 04:19:30 +0000

    lint

--- trunk/web2rss/main_loop.sh
+++ trunk/web2rss/main_loop.sh
@@ -3,8 +3,11 @@ while true; do
     (cd ..; svn update --trust-server-cert --non-interactive)
     echo SVN Done ===========`date`
 
+    start=`date`
     cp sync.sh run_sync.sh
-    sh run_sync.sh
+    time sh run_sync.sh
     #sleep 1200
+    echo "START=$start"
+    echo "  END=`date`"
     sleep 600
 done
