8d9461243399b53b8a27ced7e01c32847d3cdeed
tomei.ningen45@gmail.com <tomei.ningen45@gmail.com@10d319c2-8b8c-a289-d5c3-688514be38f6>
2015-02-08 01:43:04 +0000

    updated

--- trunk/web2rss/sync.sh
+++ trunk/web2rss/sync.sh
@@ -59,18 +59,17 @@ else
     waitscp
     $SCP data/6park.xml $root/test2.xml &
 
-  if false; then
-    tclsh 6park_forum_top.tcl
-    waitscp
-    $SCP data/6park-*.xml $root/rss/ &
+##    tclsh 6park_forum_top.tcl
+##    waitscp
+##    $SCP data/6park-*.xml $root/rss/ &
 
-    tclsh wforum.tcl
-    waitscp
-    $SCP data/wforum.xml $root/wforum.xml &
+##    tclsh wforum.tcl
+##    waitscp
+##    $SCP data/wforum.xml $root/wforum.xml &
 
-    tclsh yahoohk.tcl
-    waitscp
-    $SCP data/hkyahoo.xml $root/rss/hkyahoo.xml &
+##    tclsh yahoohk.tcl
+##    waitscp
+##    $SCP data/hkyahoo.xml $root/rss/hkyahoo.xml &
 
 #    tclsh cnbeta.tcl
 #    waitscp
@@ -80,17 +79,17 @@ else
 #   waitscp
 #   $SCP data/iza.xml $root/iza.xml &
 
-    tclsh yahoofn.tcl 
-    waitscp
-    $SCP data/yahoofn_*.xml $root/ &
+##    tclsh yahoofn.tcl 
+##    waitscp
+##    $SCP data/yahoofn_*.xml $root/ &
 
-    tclsh bloomberg_top.tcl 
-    waitscp
-    $SCP data/bloomberg_*.xml $root/ &
+##    tclsh bloomberg_top.tcl 
+##    waitscp
+##    $SCP data/bloomberg_*.xml $root/ &
 
-    tclsh zasshi.tcl 
-    waitscp
-    $SCP data/zasshi*.xml $root/rss/ &
+##    tclsh zasshi.tcl 
+##    waitscp
+##    $SCP data/zasshi*.xml $root/rss/ &
 
     tclsh gooblog_top.tcl 
     waitscp
@@ -100,31 +99,31 @@ else
 #   waitscp
 #   $SCP data/tiexue.xml $WEB2RSSROOT/rss/ &
 
-    tclsh rennlist.tcl 
-    waitscp
-    $SCP data/rennlist.xml $WEB2RSSROOT/rss/ &
+##    tclsh rennlist.tcl 
+##    waitscp
+##    $SCP data/rennlist.xml $WEB2RSSROOT/rss/ &
 
-    tclsh register.tcl 
-    waitscp
-    $SCP data/register.xml $WEB2RSSROOT/rss/ &
+##    tclsh register.tcl 
+##    waitscp
+##    $SCP data/register.xml $WEB2RSSROOT/rss/ &
 
-    tclsh craigslist.tcl 
-    waitscp
-    $SCP data/craigslist.xml $WEB2RSSROOT/rss/ &
+##    tclsh craigslist.tcl 
+##    waitscp
+##    $SCP data/craigslist.xml $WEB2RSSROOT/rss/ &
 
-    env CRAIG_LOCAL=1 tclsh craigslist.tcl 
-    dosync data/craigslist_local.xml
+##    env CRAIG_LOCAL=1 tclsh craigslist.tcl 
+##    dosync data/craigslist_local.xml
 
-    tclsh fortune.tcl 
-    waitscp
-    $SCP data/fortune.xml $WEB2RSSROOT/rss/ &
+##    tclsh fortune.tcl 
+##    waitscp
+##    $SCP data/fortune.xml $WEB2RSSROOT/rss/ &
 
-    for site in ebay_911 ebay_930 mohr pelican roadsport; do 
-        tclsh multicar.tcl $site
-        waitscp
-        $SCP data/$site.xml $WEB2RSSROOT/rss/ &
-    done
-  fi
+##    for site in ebay_911 ebay_930 mohr pelican roadsport; do 
+##        tclsh multicar.tcl $site
+##        waitscp
+##        $SCP data/$site.xml $WEB2RSSROOT/rss/ &
+##   done
+
 fi
 
     tclsh delete_old_files.tcl
