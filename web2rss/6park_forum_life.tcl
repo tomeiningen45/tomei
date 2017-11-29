ea4c5cc70ef368d0a8e28e20b435ef2b2276a6c1
tomeiningen45 <tomei.ningen45@gmail.com>
2017-11-28 22:47:41 -0800

    added 6park life

---
+++ web2rss/6park_forum_life.tcl
@@ -0,0 +1,15 @@
+# @rss-nt-adapter@
+
+namespace eval 6park_forum_life {
+    proc init {first} {
+        variable h
+        set h(article_sort_byurl) 1
+        set h(lang)  zh
+        set h(desc)  留园—生活百态
+        set h(url)   http://site.6park.com/life2
+    }
+
+    proc update_index {} {
+        ::schedule_read {6park_forum::parse_index 6park_forum_life} http://site.6park.com/life2/ gb2312
+    }
+}
