b79f245ff172c5e5e8a9b8febdcc1fb263647143
tomeiningen45 <tomei.ningen45@gmail.com>
2021-01-15 22:18:40 -0800

    try to use &nbsp; instead of wide space character

--- web2rss/hatelabo.tcl
+++ web2rss/hatelabo.tcl
@@ -51,7 +51,7 @@ namespace eval hatelabo {
 
             while {[regsub -all "<p *>\[\r\t\n \]*<p *>" $data <p> data]} {}
 
-            regsub -all <p> $data "<p>\n\n　" data
+            regsub -all <p> $data "<p>\n\n&nbsp;" data
 
             regsub -all {</h[0-9][^>]*>} $data "" data
             regsub -all {<h[0-9][^>]*>} $data "\n\n<p>■" data
