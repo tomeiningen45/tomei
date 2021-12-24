a1a700fd14672e3f035f542101aba401d2ad0301
tomeiningen45 <tomei.ningen45@gmail.com>
2021-12-24 00:26:58 -0800

    Improved caranddriver

--- web2rss/caranddriver.tcl
+++ web2rss/caranddriver.tcl
@@ -11,21 +11,30 @@ namespace eval caranddriver {
 
     proc update_index {} {
         ::schedule_read caranddriver::parse_index https://www.caranddriver.com/news/ utf-8
+        ::schedule_read caranddriver::parse_index https://www.caranddriver.com/reviews/ utf-8
+        ::schedule_read caranddriver::parse_index https://www.caranddriver.com/features/ utf-8
     }
 
     proc parse_index {index_url data} {
         foreach line [makelist $data {<div class=\"full-item}] {
             if {[regexp {<a href="([^>\"]+)"} $line dummy article_url]} {
+                set article_url https://www.caranddriver.com/$article_url
                 if {[string length $article_url] > 5 && ![db_exists caranddriver $article_url]} {
-                    ::schedule_read caranddriver::parse_article https://www.caranddriver.com/$article_url utf-8
+                    ::schedule_read caranddriver::parse_article $article_url utf-8
                 }
             }
         }
     }
 
+    proc debug_article_parser {url} {
+        ::schedule_read caranddriver::parse_article $url utf-8
+    }
+
     proc parse_article {url data} {
         set title $url
         regexp {<title>([^<]+)</title>} $data dummy title
+        regsub {.*<h1[^>]*>[^<]*</h1>} $data "" data
+
         regsub {.*<div class="content-header-inner">} $data "" data
         regsub -all {<div class="screen-reader-only">[^<]*</div>} $data "" data
 
@@ -37,6 +46,8 @@ namespace eval caranddriver {
 
         regsub -all {<span class="image-photo-credit">[^<]*</span>} $data "" data
         regsub -all {<span class="image-copyright">[^<]*</span>} $data "" data
+
+        regsub -all {<img [^>]*data-src="([^>\"]*)"[^>]*>} $data "<img width='100%' src='\\1'>" data
 
         # replace <picture> blocks
         set pic_start {<picture class="">}
@@ -50,7 +61,13 @@ namespace eval caranddriver {
         regsub -all {</span[^>]*>} $data "" data
         regsub -all "\n\[\n \t\r\]+" $data \n\n data
 
-        regsub -all {<a class="editorial-link-item"} $data "<br><li><a " data
+        regsub -all {<a class="editorial-link-item"} $data "<br><a " data
+
+        regsub {<!-- shared/end-of-content.twig -->.*} $data "" data
+
+        regsub {<button class="mobile-adhesion-unit-close-button"></button>.*} $data "" data
+
+        regsub {<img [^>]*hips.hearstapps.com/rover/profile_photos/[^>]*>} $data "" data
 
         save_article caranddriver $title $url $data
     }
@@ -58,7 +75,7 @@ namespace eval caranddriver {
     proc picture {orig} {
         if {[regexp {<source [^=]*srcset=\"([^\"]+)} $orig dummy img]} {
             regsub -all & $img \\\\\\& img
-            return "\n<br><img src='$img'><br>\n"
+            return "\n<br><img width='100%' src='$img'><br>\n"
         } else {
             return ""
         }
--- web2rss/rss-nt.tcl
+++ web2rss/rss-nt.tcl
@@ -1041,12 +1041,13 @@ proc save_article {adapter title url data {pubdate {}}} {
         puts ======================================================================
         puts $title
         puts ======================================================================
-        puts $data
+        #puts $data
 
         set fd [open out.html w+]
         puts $fd "<title>$title</title>\n"
         puts $fd "<a href=$url>$url</a><p><p>\n"
         puts $fd "<h1>TITLE: $title</h1>" 
+        puts $fd "item data follows<hr>"
         puts $fd "$data"
         close $fd
         exit
