c6b0d211db79d59a962a21dbb342700780632901
tomeiningen45 <tomei.ningen45@gmail.com>
2022-02-07 20:10:03 -0800

    added cargurus and simple json parser

---
+++ web2rss/cargurus.tcl
@@ -0,0 +1,120 @@
+# @rss-nt-adapter@
+
+namespace eval cargurus {
+    proc init {first} {
+        variable h
+        set h(article_sort_byurl) 1
+        set h(lang)  en
+        set h(desc)  {Cargurus}
+        set h(url)   http://www.cargurus.com
+    }
+
+    proc update_index {} {
+	if 0 {
+	    json_to_map $data map
+	    parray map
+	}
+
+	global g
+
+	set terms {
+	    # General
+	    &distance=50&maxPrice=20000&maxMileage=150000
+	    # 370z
+	    &distance=100&maxPrice=30000&maxMileage=150000&entitySelectingHelper.selectedEntity=d2018
+	    # Mustang
+	    &distance=100&maxPrice=30000&maxMileage=150000&entitySelectingHelper.selectedEntity=d2
+	}
+
+	regsub -all "#\[^\n\]*" $terms "" terms
+	set base https://www.cargurus.com/Cars/searchResults.action?zip=94022
+	append base &inventorySearchWidgetType=AUTO&transmission=M
+	append base &nonShippableBaseline=0&sortDir=ASC&sourceContext=carGurusHomePage_false_0
+	append base &sortType=AGE_IN_DAYS&offset=0&maxResults=35&filtersModified=true
+
+	foreach t $terms {
+	    set url ${base}${t}
+	    ::schedule_read cargurus::parse_index $url
+	}
+    }
+
+    proc format_info {prefix field {fieldname {}}} {
+	upvar map map
+	if {$fieldname == {}} {
+	    set fieldname [string toupper [string index $field 0]][string tolower [string range $field 1 end]]
+	}
+	set data ""
+	catch {
+	    set data "<tr><td><b>$fieldname:</b>&nbsp;</td> <td>$map($prefix/$field)</td></tr>\n"
+	}
+	return $data
+    }
+
+    proc parse_index {index_url data} {
+	json_to_map $data map
+
+	for {set i 0} {true} {incr i} {
+	    set prefix /$i
+	    if {![info exists map($prefix/id)]} {
+		break
+	    }
+	    set article_url https://www.cargurus.com/Cars/link/$map($prefix/id)
+	    if {[db_exists cargurus $article_url]} {
+		continue
+	    }
+	    if {[catch {
+		set price ""
+		set miles ""
+		catch {
+		    set price " \$$map($prefix/price)"
+		}
+		catch {
+		    set miles " @ $map($prefix/mileage) miles"
+		}
+		set title "$map($prefix/listingTitle)$price$miles"
+		set data ""
+		set img ""
+		catch {
+		    set img $map($prefix/originalPictureUrl)
+		    regsub "https //" $img "https://" img
+		    regsub "http //"  $img "http://"  img
+		    append data "<img src=$img><p>\n"
+		}
+		if {$img == {} || $price == {}} {
+		    # not ready yet. Try again later.
+		    continue
+		}
+		append data "<table>\n"
+		append data [format_info $prefix mileage]
+		append data [format_info $prefix price]
+		append data [format_info $prefix expectedPrice {Expected Price}]
+		append data [format_info $prefix priceDifferential {Price Diff}]
+	        append data [format_info $prefix carYear Year]
+	        append data [format_info $prefix dealScore {Deal Score}]
+		append data [format_info $prefix sellerCity {Location}]
+		append data [format_info $prefix distance]
+		append data [format_info $prefix ownerCount {Number of Owners}]
+		append data [format_info $prefix sellerType {Seller Type}]
+		append data [format_info $prefix sellerRegion {Seller State}]
+		append data [format_info $prefix sellerCity {Location}]
+		append data [format_info $prefix structuredDataDealerName {Dealer}]
+		append data [format_info $prefix makeName {Make}]
+		append data [format_info $prefix modelName {Model}]
+		append data </table>
+
+		save_article cargurus $title $article_url $data
+		#puts $title
+	    } error]} {
+		puts "$error"
+	    }
+	}
+    }
+
+    proc debug_article_parser {url} {
+        ::schedule_read cargurus::parse_article $url utf-8
+    }
+
+    proc parse_article {url data} {
+	puts "Not supported"
+    }
+}
--- web2rss/rss-lib.tcl
+++ web2rss/rss-lib.tcl
@@ -38,6 +38,93 @@ proc redirect_image {img referrer} {
     return $g(webroot)/hooks/im2/$s
 }
 
+
+# The opposite of "enclose"
+proc declose {data {n 1}} {
+    set len [string length $data]
+    set data [string range $data $n [expr $len - 2 * $n]]
+    return $data
+}
+
+# Poor man's JSON parsing
+proc json_to_map {data mapname {prefix {}}} {
+    upvar $mapname map
+    set mapname map
+
+    # Convert the [] and {}
+    # :{id:val,id:val}     = simple property list
+    #     => {-id val id val-}
+    # :[elm, elm, elm]     = simple enumerated array -> treated as simple tcl array
+    #     => { elm elm elm } 
+    # :[{id1:val,id2:val},{id1:val,id2:val}]  = treated as :{"1":{id1:val,id2:val},"2":{id1:val,id2:val}}
+    #     => {[{- elm elm elm -}]} 
+
+    set data [string trim $data]
+
+    if {"$prefix" == ""} {
+	regsub -all "\}," $data "\} " data
+
+	regsub -all "\\\[\{" $data "\ufff0" data
+	regsub -all "\}\\\]" $data "\ufff1" data
+
+	regsub -all "\\\[" $data "\ufff2" data
+	regsub -all "\\\]" $data "\ufff3" data
+
+	regsub -all "\{" $data "\ufff4" data
+	regsub -all "\}" $data "\ufff5" data
+
+	regsub -all ":" $data " " data
+	regsub -all "," $data " " data
+
+	regsub -all "\ufff0" $data "\{\[\{- " data
+	regsub -all "\ufff1" $data " -\}\]\}" data
+
+	regsub -all "\ufff2" $data "\{" data
+	regsub -all "\ufff3" $data "\}" data
+
+	regsub -all "\ufff4" $data "\{- " data
+	regsub -all "\ufff5" $data " -\}" data
+
+	#puts ==================================================
+	#puts >$data<
+	set data [declose $data 1]
+	#puts ==================================================
+	#puts $data
+	#puts ==================================================
+    }
+    if {[regexp {^\[(\{-.*-\})\]$} $data dummy data]} {
+	# enumerated property list
+	#puts $data
+	#puts ======================================================================
+	set i 0
+	foreach part $data {
+	    # remove the enclosing -
+	    set part [declose $part 1]
+	    #puts >>>$part<<<
+	    json_to_map $part $mapname $prefix/$i
+	    incr i
+	}
+    } else {
+	#puts >>$data<<
+	# simple property list
+	foreach {prop value} $data {
+	    if {[regexp {^\[(\{-.*-\})\]$} $value dummy value2]} {
+		# enumerated property list
+		json_to_map $value $mapname $prefix/$prop
+	    } elseif {[regexp {^-(.*)-$} $value dummy value]} {
+		# simple property list
+		json_to_map $value $mapname $prefix/$prop
+	    } else {
+		#puts $prefix/$prop
+		#puts "    $value"
+		set map($prefix/$prop) $value
+	    }
+	}
+    }
+}
+
+
+
 #===============================================================================
 # https://core.tcl-lang.org/tcllib/file?name=modules/ncgi/ncgi.tcl&ci=tip
 #===============================================================================
